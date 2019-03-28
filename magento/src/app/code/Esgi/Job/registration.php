6790bca1c50cf3585bf732e34bf7e80cb1629e18
Baldrani <mael.mayon@free.fr>
2019-03-28 10:07:42 +0100

    Avant de faire des folies

--- magento/src/app/code/Esgi/Job/registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-// app/code/Esgi/Job/registration.php
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Esgi_Job',
-    __DIR__
-);
