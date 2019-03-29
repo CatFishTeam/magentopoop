5572068e34f8d2bf40482ebf49adb3f2c6e17414
guillaumepn <guillaumepn@free.fr>
2019-03-29 12:26:17 +0100

    try to display freaking beer

---
+++ magento/src/app/code/Esgi/Beer/Block/Beer/View.php
@@ -0,0 +1,35 @@
+<?php
+namespace Esgi\Beer\Block;
+
+use Esgi\Beer\Model\BeerRepository;
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
+use Esgi\Beer\Model\ResourceModel\Beer\Collection;
+use Esgi\Beer\Model\ResourceModel\Beer\CollectionFactory;
+
+/**
+ * Beer block
+ */
+class View extends Template
+{
+    protected $collectionFactory;
+    protected $beerRepository;
+
+    public function __construct(
+        CollectionFactory $collectionFactory,
+        Context $context,
+        BeerRepository $beerRepository,
+        array $data = []
+    ) {
+        $this->collectionFactory = $collectionFactory;
+        $this->beerRepository = $beerRepository;
+        parent::__construct($context, $data);
+    }
+
+    public function getBeer()
+    {
+        $id = $this->getRequest()->getParam('id');
+
+        return $this->beerRepository->getById($id);
+    }
+}
