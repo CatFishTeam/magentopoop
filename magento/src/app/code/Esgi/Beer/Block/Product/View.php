6221f93a11df150577dc9f36b90977af3e040503
guillaumepn <guillaumepn@free.fr>
2019-03-29 14:56:57 +0100

    add brewery link to product

--- magento/src/app/code/Esgi/Beer/Block/Product/View.php
+++ magento/src/app/code/Esgi/Beer/Block/Product/View.php
@@ -3,6 +3,7 @@
 namespace Esgi\Beer\Block\Product;
 
 use Esgi\Beer\Model\BeerRepository;
+use Esgi\Beer\Model\BreweryRepository;
 use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Catalog\Block\Product\Context;
 
@@ -10,10 +11,13 @@ class View extends \Magento\Catalog\Block\Product\View
 {
 
     private $beerRepository;
+    private $breweryRepository;
 
-    public function __construct(Context $context, \Magento\Framework\Url\EncoderInterface $urlEncoder, \Magento\Framework\Json\EncoderInterface $jsonEncoder, \Magento\Framework\Stdlib\StringUtils $string, \Magento\Catalog\Helper\Product $productHelper, \Magento\Catalog\Model\ProductTypes\ConfigInterface $productTypeConfig, \Magento\Framework\Locale\FormatInterface $localeFormat, \Magento\Customer\Model\Session $customerSession, ProductRepositoryInterface $productRepository, \Magento\Framework\Pricing\PriceCurrencyInterface $priceCurrency, BeerRepository $beerRepository, array $data = [])
+    public function __construct(Context $context, \Magento\Framework\Url\EncoderInterface $urlEncoder, \Magento\Framework\Json\EncoderInterface $jsonEncoder, \Magento\Framework\Stdlib\StringUtils $string, \Magento\Catalog\Helper\Product $productHelper, \Magento\Catalog\Model\ProductTypes\ConfigInterface $productTypeConfig, \Magento\Framework\Locale\FormatInterface $localeFormat, \Magento\Customer\Model\Session $customerSession, ProductRepositoryInterface $productRepository, \Magento\Framework\Pricing\PriceCurrencyInterface $priceCurrency, BeerRepository $beerRepository, BreweryRepository $breweryRepository, array $data = [])
     {
         $this->beerRepository = $beerRepository;
+        $this->breweryRepository = $breweryRepository;
+
         parent::__construct($context, $urlEncoder, $jsonEncoder, $string, $productHelper, $productTypeConfig, $localeFormat, $customerSession, $productRepository, $priceCurrency, $data);
     }
 
@@ -24,5 +28,13 @@ class View extends \Magento\Catalog\Block\Product\View
         $beer = $this->beerRepository->getById($beerId);
 
         return $beer;
+    }
+
+    public function getBrewery()
+    {
+        $beerId = $this->getProduct()->getData('beer_id');
+        $beer = $this->beerRepository->getById($beerId);
+
+        return $this->breweryRepository->getById($beer->getData('brewery_id'));
     }
 }
