f0e77144c8a568cdeb759e0e414b4b998bc692b0
guillaumepn <guillaumepn@free.fr>
2019-03-29 14:42:49 +0100

    add brewery link on beer page

--- magento/src/app/code/Esgi/Beer/Block/Beer/View.php
+++ magento/src/app/code/Esgi/Beer/Block/Beer/View.php
@@ -2,6 +2,7 @@
 namespace Esgi\Beer\Block\Beer;
 
 use Esgi\Beer\Model\BeerRepository;
+use Esgi\Beer\Model\BreweryRepository;
 use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\Element\Template\Context;
 use Esgi\Beer\Model\ResourceModel\Beer\Collection;
@@ -14,15 +15,18 @@ class View extends Template
 {
     protected $collectionFactory;
     protected $beerRepository;
+    protected $breweryRepository;
 
     public function __construct(
         CollectionFactory $collectionFactory,
         Context $context,
         BeerRepository $beerRepository,
+        BreweryRepository $breweryRepository,
         array $data = []
     ) {
         $this->collectionFactory = $collectionFactory;
         $this->beerRepository = $beerRepository;
+        $this->breweryRepository = $breweryRepository;
         parent::__construct($context, $data);
     }
 
@@ -31,5 +35,13 @@ class View extends Template
         $id = $this->getRequest()->getParam('id');
 
         return $this->beerRepository->getById($id);
+    }
+
+    public function getBrewery()
+    {
+        $id = $this->getRequest()->getParam('id');
+        $beer = $this->beerRepository->getById($id);
+
+        return $this->breweryRepository->getById($beer->getData('brewery_id'));
     }
 }
